025aaa94eeabcb5be3616fcae7a25f803149ed17
iHx7z <anthony.ferreira@sptech.school>
2023-04-28 18:26:38 -0300

    dashboard

--- site sprint 2/src/database/config.js
+++ site sprint 2/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "",
+    database: "",
+    user: "",
+    password: "",
     pool: {
         max: 10,
         min: 0,
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "fungustech",
-    user: "root",
-    password: "",
+    user: "anthony",
+    password: "sptech",
 };
 
 function executar(instrucao) {
